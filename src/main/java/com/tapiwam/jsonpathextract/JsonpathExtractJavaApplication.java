12112ba282995f9fcbcc6a949f27394406c9f07e
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2023-12-01 19:38:59 -0600

    Initial commit

---
+++ src/main/java/com/tapiwam/jsonpathextract/JsonpathExtractJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tapiwam.jsonpathextract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonpathExtractJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonpathExtractJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tapiwam/jsonpathextract/JsonpathExtractJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapiwam.jsonpathextract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonpathExtractJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
